0e957d7878aa46fb88bf846d76b601edda7b17f3
wahyulamani03 <wahyulamani03@gmail.com>
2026-04-20 02:05:44 +0000

    change design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-'use client'
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const route = useRouter()
-  route.replace("/dashboard")
   return (
     <>
       <h1>Home</h1>
