c0c5cc94ba9ea442fc70bd1e39c454d144cdfc68
wahyulamani03 <wahyulamani03@gmail.com>
2026-04-21 11:53:22 +0000

    fix bug arrow back

--- components/utils/SubHeader.tsx
+++ components/utils/SubHeader.tsx
@@ -1,21 +1,28 @@
+"use client";
 import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-
-export default function SubHeader({children}: Readonly<{children: React.ReactNode}>){
-    return(
-        <>
-        <header className=" sticky top-0 z-50 bg-[#fff9ec] dark:bg-[#1e1c10]">
-          <div className="flex items-center px-6 py-4 justify-between ">
-            <div className="flex items-center gap-4">
-              <button className="hover:opacity-80 transition-opacity active:scale-95 duration-200">
-                <span className="material-symbols-outlined text-[#442a22] dark:text-[#ece0dc]">
+export default function SubHeader({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+  return (
+    <>
+      <header className=" sticky top-0 z-50 bg-[#fff9ec] dark:bg-[#1e1c10]">
+        <div className="flex items-center px-6 py-4 justify-between ">
+          <div className="flex items-center gap-4">
+            <button
+              className="hover:opacity-80 transition-opacity active:scale-95 duration-200"
+              onClick={() => router.back()}
+            >
+              <span className="material-symbols-outlined text-[#442a22] dark:text-[#ece0dc]">
                 <ArrowLeft />
-                </span>
-              </button>
-              {children}
-            </div>
+              </span>
+            </button>
+            {children}
           </div>
-        </header>
-        </>
-    )
+        </div>
+      </header>
+    </>
+  );
 }
