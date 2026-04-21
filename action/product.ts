1f2e9ad59054c54ba90afab24462840203719cff
wahyulamani03 <wahyulamani03@gmail.com>
2026-04-21 11:20:11 +0000

    add photo and upload on storage provider supabase

--- action/product.ts
+++ action/product.ts
@@ -1,13 +1,38 @@
-"use server"
+"use server";
 import { prisma } from "@/lib/prisma";
+import { supabase } from "@/lib/supabase";
 import { ProductSchema } from "@/lib/validation/product";
 
-export async function createProduct(data: unknown){
-    const parsed = ProductSchema.safeParse(data);
-    if(!parsed.success){
-        return { success: false, errors: parsed.error.flatten().fieldErrors }
-    }
+export async function createProduct(formData: FormData) {
+  const file = formData.get("image") as File | null;
 
-    await prisma.products.create({data: parsed.data})
-    return {success: true, message:"Produk berhasil ditambahkan"}
+  let image_url: string | undefined;
+  if (file && file.size > 0) {
+    const ext = file.name.split(".").pop();
+    const fileName = `${Date.now()}-${Math.random()
+      .toString(36)
+      .slice(2)}.${ext}`;
+    const { error } = await supabase.storage
+      .from("product_photo")
+      .upload(fileName, file, { contentType: file.type, upsert: false });
+    if (error)
+      return { success: false, message: { image_url: [error.message] } };
+    const { data: publicUrl } = supabase.storage
+      .from("product_photo")
+      .getPublicUrl(fileName);
+    image_url = publicUrl.publicUrl;
+  }
+  const raw = {
+    name: formData.get("name"),
+    category_id: formData.get("category_id"),
+    unit: formData.get("unit"),
+    selling_price: formData.get("selling_price"),
+    image_url,
+  };
+  const parsed = ProductSchema.safeParse(raw);
+  if (!parsed.success)
+    return { success: false, errors: parsed.error.flatten().fieldErrors };
+
+  await prisma.products.create({ data: parsed.data });
+  return { success: true, message: "Produk berhasil ditambahkan" };
 }
